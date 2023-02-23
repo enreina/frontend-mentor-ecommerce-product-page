dfc17a06380b26cd981789ab550afb51088f8880
Enreina <enreina@gmail.com>
2023-02-23 19:24:44 +0700

    Responsive header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,17 +14,24 @@ const navigationLinks: NavigationLink[] = [
 </script>
 
 <template>
-    <header class="container mx-auto flex flex-row space-x-8 border-b box-content">
-        <a class="flex items-center py-8" href="#"><img class="py-2" height="20px" src="../assets/logo.svg" /></a>
-        <nav class="flex flex-grow space-x-4 items-center">
+    <header class="px-6 md:px-0 flex flex-row space-x-8 md:border-b box-content">
+        <button class="md:hidden fill-dark-gray-blue hover:fill-dark-blue">
+            <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg"><path d="M16 12v3H0v-3h16Zm0-6v3H0V6h16Zm0-6v3H0V0h16Z" fill-rule="evenodd"/></svg>
+        </button> 
+        <a class="grow md:grow-0 flex items-center py-4 md:py-8" href="#"><img class="py-2" height="20px" src="../assets/logo.svg" /></a>
+        <nav class="hidden md:flex grow space-x-4 items-center">
             <a v-for="link in navigationLinks" 
-            class="py-9 text-darkGrayBlue border-b-transparent border-b-4 text-base
-                hover:text-darkBlue hover:border-b-orange" 
+            class="py-9 text-dark-gray-blue border-b-transparent border-b-4 text-base
+                hover:text-dark-blue hover:border-b-orange" 
             href="#">
                 {{ link.title }}
             </a>
         </nav>
-        <button><img src="../assets/icon-cart.svg"></button>
-        <button><img class="w-14 h-14" src="../assets/image-avatar.png"></button>
+        <button>
+            <svg class="fill-dark-gray-blue hover:fill-dark-blue" width="22" height="20" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill-rule="nonzero"/>
+            </svg>
+        </button>
+        <button><img class="rounded-full border-2 border-transparent hover:border-orange w-14 h-14" src="../assets/image-avatar.png" /></button>
     </header>
 </template>
