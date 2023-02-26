9eeae424dc167dac4b45001ef1244677cb9ef6fa
Enreina <enreina@gmail.com>
2023-02-26 17:39:26 +0700

    Add to cart button (presentational)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
     extend: {
       colors: {
         orange: "hsl(26, 100%, 55%)",
+        "light-orange": "hsl(27, 100%, 71%)",
         "pale-orange": "hsl(25, 100%, 94%)",
         "dark-blue": "hsl(220, 13%, 13%)",
         "dark-gray-blue": "hsl(219, 9%, 45%)",
