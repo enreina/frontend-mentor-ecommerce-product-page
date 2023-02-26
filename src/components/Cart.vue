3fe4b299f30152159d911bfba16ee4407bb7af65
Enreina <enreina@gmail.com>
2023-02-26 15:08:22 +0700

    Click cart instead of hover; Add cart open animation

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-    <div v-if="isOpen" class="absolute left-0 right-0 mx-2 mt-2 md:w-96 md:left-auto top-full bg-white rounded-lg drop-shadow-xl">
+    <div class="absolute left-0 right-0 mx-2 mt-2 md:w-96 md:left-auto top-full bg-white rounded-lg drop-shadow-xl transition-[transform,visibility]"
+         :class="{'-translate-y-full': !isOpen, invisible: !isOpen, 'translate-y-0': isOpen}">
         <h1 class="text-md font-bold text-dark-blue p-6 border-b">Cart</h1>
         <div class="py-20 flex justify-center">
             <p class="text-dark-gray-blue text-md font-bold">Your cart is empty.</p>
