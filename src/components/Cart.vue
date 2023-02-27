8f44c5d9bf21011ea4b0a716f94f18617b66c982
Enreina <enreina@gmail.com>
2023-02-27 20:08:53 +0700

    Fix z-index of cart to be on top of image slider

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -19,7 +19,7 @@ function getTotalPriceAsStr(product: Product, amount: number) {
 </script>
 
 <template>
-    <div class="absolute left-0 right-0 mx-2 mt-2 md:w-96 md:left-auto top-full bg-white rounded-lg drop-shadow-xl transition-[transform,visibility]"
+    <div class="absolute left-0 right-0 mx-2 mt-2 md:w-96 md:left-auto top-full bg-white rounded-lg drop-shadow-xl transition-[transform,visibility] z-50"
          :class="{'-translate-y-full': !isOpen, invisible: !isOpen, 'translate-y-0': isOpen}">
         <h1 class="text-md font-bold text-dark-blue p-6 border-b">Cart</h1>
         <div v-if="store.cart.length === 0" class="py-20 flex justify-center">
